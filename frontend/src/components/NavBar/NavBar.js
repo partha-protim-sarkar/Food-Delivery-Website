172576f57f0792f2f5d9287899454972da917759
partha-protim-sarkar <parthaprotims681@gmail.com>
2024-08-30 00:47:24 +0530

    make website responsive

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -12,7 +12,6 @@ const NavBar = () => {
         <ul className='navbar-menu'>
             <li onClick={() => setMenu('home')} className={menu === 'home' ? 'active' : ""}>Home</li>
             <li onClick={() => setMenu('menu')} className={menu === 'menu' ? 'active' : ""}>Menu</li>
-            <li onClick={() => setMenu('mobile-app')} className={menu === 'mobile-app' ? 'active' : ""}>Mobile App</li>
             <li onClick={() => setMenu('contact-us')} className={menu === 'contact-us' ? 'active' : ""}>Contact Us</li>
         </ul>
         <div className='navbar-right'>
