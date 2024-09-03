96f80bfdb54746bf14ced82298e8400149006f89
partha-protim-sarkar <parthaprotims681@gmail.com>
2024-09-03 22:14:33 +0530

    add login popup to website UI

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -3,7 +3,7 @@ import './NavBar.css';
 import { assets } from '../../assets/assets';
 import { Link } from 'react-router-dom';
 
-const NavBar = () => {
+const NavBar = ({setShowLogin}) => {
 
     const [menu, setMenu] = useState('home');
 
@@ -21,7 +21,7 @@ const NavBar = () => {
                 <img src={assets.basket_icon} alt='' />
                 <div className='dot'></div>
             </div> 
-            <button>Login</button>   
+            <button onClick={() => setShowLogin(true)}>Login</button>   
         </div>
     </div>
   );
