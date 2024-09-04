4d4fdf75b9c0da1d315bc18ce85f2b97b3b9ecaf
partha-protim-sarkar <parthaprotims681@gmail.com>
2024-09-04 22:16:13 +0530

    add total price calculation and indicator for item presence in cart

--- frontend/src/pages/Cart/Cart.js
+++ frontend/src/pages/Cart/Cart.js
@@ -4,7 +4,7 @@ import { StoreContext } from '../../context/StoreContext';
 
 const Cart = () => {
 
-  const {cartItems, food_list, removeFromCart} = useContext(StoreContext);
+  const {cartItems, food_list, removeFromCart, getTotalCartAmount} = useContext(StoreContext);
 
   return (
     <div className='cart'>
@@ -44,17 +44,17 @@ const Cart = () => {
           <div>
             <div className='cart-total-details'>
               <p>Subtotal</p>
-              <p>₹{0}</p>
+              <p>₹{getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className='cart-total-details'>
-              <p>Delivery Fee</p>
-              <p>₹{30}</p>
+              <p>GST</p>
+              <p>₹{getTotalCartAmount()*0.05}</p>
             </div>
             <hr />
             <div className='cart-total-details'>
               <b>Total</b>
-              <b>₹{30}</b>
+              <b>₹{getTotalCartAmount() + getTotalCartAmount()*0.05}</b>
             </div>
           </div>
           <button>PROCEED TO CHECKOUT</button>
